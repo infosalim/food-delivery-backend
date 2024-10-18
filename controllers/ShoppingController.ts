a0f82beaa4698807022d5ec714648aacb4be5579
infosalim <salimh5757@gmail.com>
2024-10-19 02:22:40 +0600

    feat(shopping): - Get available foods

---
+++ controllers/ShoppingController.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from 'express';
+import { Vendor } from '../models';
+
+export const GetFoodAvailability = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({
+    pincode: pincode,
+    serviceAvailable: false,
+  })
+    .sort([['rating', 'descending']])
+    .populate('foods');
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(400).json({ message: 'Data not found' });
+};
+
+export const GetTopRestaurants = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+export const GetFoodsIn30Min = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+export const SearchFoods = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+export const RestaurantById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
