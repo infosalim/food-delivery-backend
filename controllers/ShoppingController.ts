2bbd42f28772f52d6674959fc583df25ea8353d0
infosalim <salimh5757@gmail.com>
2024-10-21 16:48:25 +0600

    feat(shopping): Foods in 30

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { Vendor } from '../models';
+import { FoodDoc, Vendor } from '../models';
 
 export const GetFoodAvailability = async (
   req: Request,
@@ -47,7 +47,27 @@ export const GetFoodsIn30Min = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({
+    pincode: pincode,
+    serviceAvailable: false,
+  }).populate('foods');
+
+  if (result.length > 0) {
+    let foodResult: any = [];
+
+    result.map((vendor) => {
+      const foods = vendor.foods as unknown as [FoodDoc];
+
+      foodResult.push(...foods.filter((food) => food.readyTime <= 30));
+    });
+    return res.status(200).json(foodResult);
+  }
+
+  return res.status(400).json({ message: 'Data not found' });
+};
 export const SearchFoods = async (
   req: Request,
   res: Response,
